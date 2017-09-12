5d74ea22353bababafa304dea68e438a6579441f
PHOLNAJA <nathaphol_008@windowslive.com>
2017-09-12 16:46:44 +0700

    set id to input login

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,7 +8,7 @@
 				<form @submit.prevent="login(user, pass)">
 					<div class="field">
 					  <p class="control has-icons-left has-icons-right">
-					    <input class="input is-large" type="text" placeholder="Username" required v-model="user">
+					    <input class="input is-large" type="text" id="user" placeholder="Username" required v-model="user">
 					    <span class="icon is-medium is-left">
 					      <i class="fa fa-user"></i>
 					    </span>
@@ -18,7 +18,7 @@
 					</div>
 					<div class="field">
 					  <p class="control has-icons-left">
-					    <input class="input is-large" type="password" placeholder="Password" required v-model="pass">
+					    <input class="input is-large" type="password" id="pwd" placeholder="Password" required v-model="pass">
 					    <span class="icon is-medium is-left">
 					      <i class="fa fa-lock"></i>
 					    </span>
@@ -26,7 +26,7 @@
 					</div>
 					<div class="field">
 					  <p class="control">
-					    <button class="button is-success is-large" style="width:100%;" type="submit">
+					    <button class="button is-success is-large" id="login" style="width:100%;" type="submit">
 					      Login
 					    </button>
 					  </p>
